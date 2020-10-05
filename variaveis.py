d552221b4a98eec71633c3cc537962ae2a8c1acb
vitorAmorims <vtamorims@gmail.com>
2020-10-05 09:49:17 -0300

    Aula variaveis python

---
+++ variaveis.py
@@ -0,0 +1,42 @@
+#As variáveis ​​não precisam ser declaradas com nenhum tipo específico e podem até mesmo mudar de tipo após terem sido definidas.
+x = 4
+x = "vitor"
+#print(x)
+
+#Variáveis ​​de string podem ser declaradas usando aspas simples ou duplas:
+x = 'Lucas'
+print(x)
+
+
+#Regras para variáveis ​​Python:
+'''
+Lembre-se de que os nomes das variáveis ​​diferenciam maiúsculas de minúsculas
+
+Um nome de variável deve começar com uma letra ou o caractere de sublinhado
+Um nome de variável não pode começar com um número
+Um nome de variável pode conter apenas caracteres alfanuméricos e sublinhados (Az, 0-9 e _)
+Os nomes das variáveis ​​diferenciam maiúsculas de minúsculas (idade, idade e IDADE são três variáveis ​​diferentes)
+'''
+#Legal variable names:
+myvar = "John"
+my_var = "John"
+_my_var = "John"
+myVar = "John"
+MYVAR = "John"
+myvar2 = "John"
+print (myvar)
+
+#atribuindo valor a variavéis 
+
+aluno, aluno1, aluno2 = 'Vitor', 'Lucas', 'Silvia'
+#print(aluno, aluno1, aluno2)
+
+casa = casa1 = casa2 = 'Rua alencar' #achei muito interessantte está maneira de atribuir
+print(casa)
+print(casa1)
+print(casa2)
+
+#concatenção
+print('Eu tenho residência: '+ casa)
+print(myvar+casa) #aqui estou concatenando duas variaveis tipo string
+
