e68c0cc61bc17656081407c3bc79aba021a74c8e
vitorAmorims <vtamorims@gmail.com>
2020-10-05 10:06:25 -0300

    criando variavel global dentro de uma funçao

--- variaveis_globais.py
+++ variaveis_globais.py
@@ -9,3 +9,16 @@ myFunc() #chamando função
 
 #Se você criar uma variável com o mesmo nome dentro de uma função, essa variável será local e só pode ser usada dentro da função.
 
+'''
+ormalmente, quando você cria uma variável dentro de uma função, essa variável é local e só pode ser usada dentro dessa função.
+
+Para criar uma variável global dentro de uma função, você pode usar a globalpalavra - chave.
+'''
+def praise():
+    global God
+    God = 'wonderful'
+    print(God)
+praise() #chamei função
+
+print('My God is: ' + God) # aqui estou chamando a variavel global que foi criada dentro de uma função.
+
