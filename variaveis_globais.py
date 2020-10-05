e402af5d9c68047b0bd4323f47479d6aa00e9159
vitorAmorims <vtamorims@gmail.com>
2020-10-05 10:00:36 -0300

    criando variavel local dentro da funçao

--- variaveis_globais.py
+++ variaveis_globais.py
@@ -2,6 +2,10 @@
 x = 'awesome' #variable global
 
 def myFunc():
+    x = 'fantastic'
     print('Python is '+ x) #concatenção de string + string da variavel global
 
 myFunc() #chamando função
+
+#Se você criar uma variável com o mesmo nome dentro de uma função, essa variável será local e só pode ser usada dentro da função.
+
